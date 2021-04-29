c5ed7a8a9ff19c783468c3b687cf71f07ffdb5e4
ThinkBIG-Company <developers@think-big-company.com>
2021-04-29 15:33:55 +0200

    Dev

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    initializeReflectable();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
